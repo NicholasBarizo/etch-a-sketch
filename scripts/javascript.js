305eafc90d1aa0a280734dd3bab52c6515f707ee
Nicholas <nicholasbarizo@gmail.com>
2020-12-10 01:58:23 -0800

    Created div that takes user height/width input  and assigns them to variables

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,15 +1,25 @@
 const etchGrid = document.getElementById('etchGrid');
 const resetButton = document.getElementById('resetButton');
-
+const inputDimensions = document.getElementById('inputDimensions');
+const confirmGridDimensions = document.getElementById('confirmGridDimensions');
+const inputDimensionButtons = document.getElementById('inputDimensionButtons');
 createGrid();
+
+let x = document.getElementById('gridHeightInput');
 
 resetButton.addEventListener('click', () => {
     const etchGridCell = document.querySelectorAll('div.etchGridCell');
+    etchGrid.style.display = 'none';
+    inputDimensions.style.display = 'flex';
     for (i = 0; i <= 16; i++){
         etchGridCell.forEach(function(element){
             element.style.backgroundColor = 'white'
         });
     }
+});
+confirmGridDimensions.addEventListener('click', () => {
+    let gridHeight = inputDimensionButtons[0].value;
+    let gridWidth = inputDimensionButtons[1].value;
 });
 function createGrid(){
     for(i = 1; i <= 16; i++){
