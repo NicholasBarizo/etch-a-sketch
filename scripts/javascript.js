f8fd58f3501c3214007b0f33eed0f86ab541c705
Nicholas <nicholasbarizo@gmail.com>
2020-12-10 00:18:42 -0800

    Added a reset button that clears each colored cell

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,14 +1,25 @@
 const etchGrid = document.getElementById('etchGrid');
-
+const resetButton = document.getElementById('resetButton');
 
 createGrid();
-etchGrid.addEventListener('mouseover', (event) => {
-    event.target.style.backgroundColor = 'black';
+
+resetButton.addEventListener('click', () => {
+    const etchGridCell = document.querySelectorAll('div.etchGridCell');
+    for (i = 0; i <= 16; i++){
+        etchGridCell.forEach(function(element){
+            element.style.backgroundColor = 'white'
+        });
+    }
 });
 function createGrid(){
     for(i = 1; i <= 16; i++){
         const newGridCell = document.createElement('div');
         newGridCell.classList.add('etchGridCell');
+        newGridCell.id = 'cell' + i;
+        newGridCell.addEventListener('mouseover', (event) => {
+            newGridCell.style.backgroundColor = 'black';
+        });
         etchGrid.appendChild(newGridCell);
+
     }
 }
